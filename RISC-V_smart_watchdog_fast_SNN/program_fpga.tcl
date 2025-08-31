cdbc6f2c2904b64f65d42f539b70855828ab815c
David Simpson <100368901+DS-567@users.noreply.github.com>
2025-08-31 22:26:57 +0100

    Update program_fpga.tcl

--- RISC-V_smart_watchdog_fast_SNN/program_fpga.tcl
+++ RISC-V_smart_watchdog_fast_SNN/program_fpga.tcl
@@ -1,23 +1,22 @@
-open_hw
+# locate bitstream in folder
+set script_dir [file dirname [info script]]
+set bitfile [file join $script_dir "riscv_watchdog_fast_design_2_compressed.bit"]
 
-# Connect to local hw_server
+# 2. open hardware manager and connect to FPGA
+open_hw_manager
 connect_hw_server
-
-# Auto connect to the first available target
 open_hw_target
 
-# Pick the first FPGA device found
+# 3. select the first FPGA 
 set my_device [lindex [get_hw_devices] 0]
 current_hw_device $my_device
 refresh_hw_device $my_device
 
-# Set your bitstream path
-set_property PROGRAM.FILE {C:/riscv_watchdog_fast_design_2/riscv_watchdog_fast_design_2_compressed.bit} $my_device
-
-# Program the device
+# 4. program FPGA with compressed bitstream
+set_property PROGRAM.FILE $bitfile $my_device
 program_hw_devices $my_device
 
-# (Optional) Refresh after programming
+# 5. refresh FPGA
 refresh_hw_device $my_device
 
 puts "FPGA programmed :)"
