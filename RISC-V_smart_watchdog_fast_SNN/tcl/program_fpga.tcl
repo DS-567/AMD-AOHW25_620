cae4a6974c747d8a79a9d0ba9c041063533367a6
David Simpson <100368901+DS-567@users.noreply.github.com>
2025-08-31 21:49:16 +0100

    Update program_fpga.tcl

--- RISC-V_smart_watchdog_fast_SNN/tcl/program_fpga.tcl
+++ RISC-V_smart_watchdog_fast_SNN/tcl/program_fpga.tcl
@@ -1,20 +1,23 @@
+# close and open hardware manager
+close_hw
 open_hw
 
-# Connect to local hw_server
+# connect to local hw_server
 connect_hw_server
 
-# Auto connect to the first available target
+# auto connect
 open_hw_target
 
-# Pick the first FPGA device found
+# select first FPGA device found
 set my_device [lindex [get_hw_devices] 0]
 current_hw_device $my_device
 refresh_hw_device $my_device
 
-# Set your bitstream path
-set_property PROGRAM.FILE {C:/riscv_watchdog_fast_design_2/riscv_watchdog_fast_design_2_compressed.bit} $my_device
+# Set bitstream path
+cd C:/riscv_watchdog_fast_design_2
+set_property PROGRAM.FILE {riscv_watchdog_fast_design_2_compressed.bit} $my_device
 
-# Program the device
+# program FPGA
 program_hw_devices $my_device
 
 # (Optional) Refresh after programming
