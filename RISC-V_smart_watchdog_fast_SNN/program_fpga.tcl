6887eed52191388ca4a98b3d670b857559aa1022
David Simpson <100368901+DS-567@users.noreply.github.com>
2025-08-31 23:02:22 +0100

    Update program_fpga.tcl

--- RISC-V_smart_watchdog_fast_SNN/program_fpga.tcl
+++ RISC-V_smart_watchdog_fast_SNN/program_fpga.tcl
@@ -1,6 +1,7 @@
 # locate bitstream in folder
 set script_dir [file dirname [info script]]
-set bitfile [file join $script_dir "riscv_watchdog_fast_design_2_compressed.bit"]
+set bit_path [file join $script_dir "riscv_watchdog_fast_design_2_compressed.bit"]
+set ila_path [file join $script_dir "debug_nets.ltx"]
 
 # open hardware manager and connect to FPGA
 open_hw_manager
@@ -12,24 +13,12 @@ set my_device [lindex [get_hw_devices] 0]
 current_hw_device $my_device
 refresh_hw_device $my_device
 
-# verify it is the VC709 Virtex-7 connected
-set dev_name [get_property PART $my_device]
-set expected_part "xc7vx690tffg1761-2L"
-
-if {$dev_name != $expected_part} {
-    puts "ERROR: This FPGA ($dev_name) is not the expected VC709 device ($expected_part)."
-    puts "Programming aborted - wrong FPGA connected!"
-    exit
-}
-
-# 4. program FPGA with compressed bitstream
-set_property PROGRAM.FILE $bitfile $my_device
+# program FPGA with compressed bitstream
+set_property PROGRAM.FILE $bit_path $my_device
+set_property PROBES.FILE $ila_path $my_device
 program_hw_devices $my_device
 
-# 5. refresh FPGA
+# refresh FPGA
 refresh_hw_device $my_device
 
 puts "FPGA programmed :)"
-
-
-
